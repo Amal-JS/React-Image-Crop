5d8a9e469b2e2eccfbcfd277ec6c03d5dc903045
amaljs <amaljs121@gmail.com>
2024-03-06 12:03:31 +0530

    inital commit

---
+++ reactImageCrop/src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='h-screen w-full bg-gray-800 text-center'>
+          <h2 className='text-3xl text-white py-8 font-bold'>Image Crop</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
